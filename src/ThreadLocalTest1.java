d5f25e92401638154b7d80dd4592a1634c881ed6
TBirdsWR <threebee0226@163.com>
2020-10-30 15:10:30 +0800

    test commit

--- src/ThreadLocalTest1.java
+++ src/ThreadLocalTest1.java
@@ -1,2 +1,21 @@
 public class ThreadLocalTest1 {
+
+    private Person person;
+    private ThreadLocal threadLocal = new ThreadLocal();
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Person{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
